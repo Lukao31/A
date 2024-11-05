b48a81b71038d7b3fda3e33098b469123197f12a
LucasLimaprz <burngames800@gmail.com>
2024-11-05 08:57:43 -0300

    ExercíciosSozinho2

---
+++ Cap3/exercícios/ex2/exercicio2.js
@@ -0,0 +1,20 @@
+function verificadorVelocidade() {
+    // Cria uma referência aos elementos da página
+    let inPermitida = document.getElementById("inPermitida")
+    let inCondutor = document.getElementById("inCondutor")
+    let outResposta = document.getElementById("outResposta")
+
+    // Obtém o conteúdo dos campos
+    let velocidadePermitida = Number(inPermitida.value)
+    let velocidadeCondutor = Number(inCondutor.value)
+
+    // Operação do Verificador de velocidade
+    if(velocidadeCondutor > velocidadePermitida) {
+        outResposta.textContent = "Situação: Multa leve" 
+    } 
+    
+    if(velocidadeCondutor <= velocidadePermitida) {
+        outResposta.textContent = "Situação: Sem multas, continue assim!"
+    }
+    
+}
